12c67bfd6c64ba73b49d2c4064ce1e017ea4d976
Nishant Singh <nraj14908@gmail.com>
2026-04-17 16:02:02 +0530

    feat(backend): done with the signin endpoint

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,16 +1,14 @@
 
 import express from "express"
-import json from "jsonwebtoken"
-const Secreat="nishuinlove"
+import jwt from "jsonwebtoken"
 import {UserModel} from "db/client"
 import mongoose from "mongoose"
 mongoose.connect(process.env.MONGO_URL!)
 import bcrypt from "bcrypt"
-import { z } from "zod"
-
-import { userSignUpSchema } from "common/types"
+import { userSignUpSchema, userSignIpSchema } from "common/types"
 
 const app = express()
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
     try {
@@ -54,9 +52,56 @@ app.post("/signup", async (req, res) => {
     }
 })
 
-app.post("/signin", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+app.post("/signin", async (req, res) => {
+    try {
+        const parseData = userSignIpSchema.safeParse(req.body)
+        
+        if(!parseData.success) {
+            return res.status(401).json({
+                message: "Invalid Input",
+                error: parseData.error.flatten()
+            })
+        }
+
+        const { email, password } = parseData.data
+
+        const user = await UserModel.findOne({
+            email: email
+        })
+
+        if(!user) {
+            return res.status(402).json({
+                message: "user not found"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(!isMatch) {
+            return res.status(401).json({
+                message: "incorrect Credentials"
+            })
+        }
+
+        const token = jwt.sign(
+            {userId: user._id},
+            process.env.JWT_SECERTE as string,
+            {
+               expiresIn: "7d"
+            }
+        );
+
+        return res.status(200).json({
+            message: "Login successful",
+            token
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            message: "Something went wrong",
+            errors: err instanceof Error ? err.message : "Unknown error"
+        })
+    }
 })
 
 app.put("/workflow", (req, res) => {
@@ -71,7 +116,7 @@ app.post("/workflow/execution/:workflowId", (req, res) => {
 
 })
 
-app.post("/credentials", (req, res) => {
+app.get("/nodes", (req, res) => {
 
 })
 
