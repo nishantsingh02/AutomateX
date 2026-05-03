bff92f40de9ee699a46bfc7b29dddc4ecb261c98
Nishant Singh <nraj14908@gmail.com>
2026-05-03 19:06:22 +0530

    done with fixes

--- .history/apps/client/src/lib/api_20260503190313.ts
+++ .history/apps/client/src/lib/api_20260503190313.ts
@@ -3,7 +3,7 @@ import type { InternalAxiosRequestConfig, AxiosError } from "axios";
 
 
 // --- Constants ---
-const BACKEND_URL = "http://localhost:3000";
+// const BACKEND_URL = "http://localhost:3000";
 // const BACKEND_URL = "https://automate-x-backend.vercel.app"; // production url
 const TOKEN_KEY = "trading_auth_token";
 
